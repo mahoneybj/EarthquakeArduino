a6588e3275c9edef91e6415b120cae0771dcac5e
mahoneybj <benmahoney10@gmail.com>
2024-06-10 09:43:03 +1200

    Basic database created

---
+++ app.js
@@ -0,0 +1,48 @@
+// Import the Express module
+import express from "express";
+
+// Import the CORS module
+import cors from "cors";
+
+
+const app = express();
+// This should be declared under const app = express();
+const setXContentTypeOptions = (req, res, next) => {
+  res.set("x-content-type-options", "nosniff");
+  next();
+};
+// This should be declared under the setXContentTypeOptions function
+const setXFrameOptions = (req, res, next) => {
+  res.set("x-frame-options", "deny");
+  next();
+};
+const setContentSecurityPolicy = (req, res, next) => {
+  res.set("content-security-policy", "default-src 'none'");
+  next();
+};
+
+// This should be declared under app.use(cors());
+app.use(setXContentTypeOptions);
+// This should be declared under app.use(setXContentTypeOptions);
+app.use(setXFrameOptions);
+app.use(setContentSecurityPolicy);
+
+// Use the CORS module
+app.use(cors());
+// This should be declared under app.use(cors());
+app.use(express.urlencoded({ extended: false })); // To parse the incoming requests with urlencoded payloads. For example, form data
+// This should be declared under app.use(urlencoded({ extended: false }));
+app.use(express.json()); // To parse the incoming requests with JSON payloads. For example, REST API requests
+
+
+app.get("*", (req, res) => {
+    res.status(404).json({ msg: "Endpoint does not exist" });
+  });
+  
+  // Start the server on port 3000
+  app.listen(3000, () => {
+    console.log("Server is listening on port 3000.");
+  });
+  
+  // Export the Express application. Other modules may use it. For example, API testing
+  export default app;
