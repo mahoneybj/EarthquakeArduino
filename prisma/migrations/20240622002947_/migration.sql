d36ad8055c7b002a11ea9693d006a1ed43696781
Ben Mahoney <benmahoney10@gmail.com>
2024-06-22 12:30:35 +1200

    Changed prisma

--- prisma/migrations/20240622002947_/migration.sql
+++ prisma/migrations/20240622002947_/migration.sql
@@ -13,9 +13,9 @@ CREATE TABLE "RawData" (
 CREATE TABLE "EarthquakeAlert" (
     "id" SERIAL NOT NULL,
     "timestamp" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "pwave" TIMESTAMP(3) NOT NULL,
-    "swave" TIMESTAMP(3) NOT NULL,
-    "max" DECIMAL(65,30) NOT NULL,
+    "pwave" DECIMAL(65,30) NOT NULL,
+    "swave" DECIMAL(65,30) NOT NULL,
+    "sensorid" INTEGER NOT NULL,
 
     CONSTRAINT "EarthquakeAlert_pkey" PRIMARY KEY ("id")
 );
